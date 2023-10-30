e93e292e4618f521163441a94ba7ce6a3eec1463
alisadaintanvir <alisadaintanvir@gmail.com>
2023-10-31 01:56:15 +0600

    complete project

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def req_api_data(query_char):
@@ -12,8 +13,10 @@ def req_api_data(query_char):
 
 def get_password_leaks_count(hashes, hash_to_check ):
     hashes = (line.split(':') for line in hashes.text.splitlines())
-    for h in hashes:
-        print(h)
+    for h,count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
 
 
 def pwned_api_check(password):
@@ -25,4 +28,15 @@ def pwned_api_check(password):
     return get_password_leaks_count(response, tail)
 
 
-pwned_api_check('123')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times... you should change the password')
+        else:
+            print(f'{password} was Not found. Carry on!')
+    return 'done'
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
